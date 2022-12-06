ac0fad80508576f37cea51d33b2051a3085464f8
zianu <hunnybofficial@gmail.com>
2022-12-06 10:38:34 +0300

    First Commit

---
+++ src/main/java/net/azuremods/mtiwta/MTIWTA.java
@@ -0,0 +1,39 @@
+package net.azuremods.mtiwta;
+
+import com.mojang.logging.LogUtils;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(MTIWTA.MOD_ID)
+public class MTIWTA
+{
+    public static final String MOD_ID = "mtiwta";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public MTIWTA()
+    {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::commonSetup);
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+    }
+
+    @Mod.EventBusSubscriber(modid = MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents
+    {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+
+        }
+    }
+}
