6dbe6b1261eb927295648487cf3779438dc06ad6
zianu <hunnybofficial@gmail.com>
2022-12-09 12:53:06 +0300

    Added new blocks

--- src/main/java/net/azuremods/mtiwta/block/ModBlocks.java
+++ src/main/java/net/azuremods/mtiwta/block/ModBlocks.java
@@ -6,8 +6,7 @@ import net.azuremods.mtiwta.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.Material;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -24,12 +23,56 @@ public class ModBlocks {
     public static final RegistryObject<Block> POLISHED_GRANITE_BRICKS = registerBlock("polished_granite_bricks",
             ()-> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_GRANITE_BRICKS_STAIRS = registerBlock("polished_granite_bricks_stairs",
+            ()-> new StairBlock(() -> ModBlocks.POLISHED_GRANITE_BRICKS.get().defaultBlockState() ,
+                    BlockBehaviour.Properties.of(Material.STONE).strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)),
+            ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_GRANITE_BRICKS_SLAB = registerBlock("polished_granite_bricks_slab",
+            ()-> new SlabBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_GRANITE_BRICKS_WALL = registerBlock("polished_granite_bricks_wall",
+            ()-> new WallBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_GRANITE_WALL = registerBlock("polished_granite_wall",
+            ()-> new WallBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+
     public static final RegistryObject<Block> POLISHED_ANDESITE_BRICKS = registerBlock("polished_andesite_bricks",
             ()-> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_ANDESITE_BRICKS_STAIRS = registerBlock("polished_andesite_bricks_stairs",
+            ()-> new StairBlock(() -> ModBlocks.POLISHED_GRANITE_BRICKS.get().defaultBlockState() ,
+                    BlockBehaviour.Properties.of(Material.STONE).strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)),
+            ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_ANDESITE_BRICKS_SLAB = registerBlock("polished_andesite_bricks_slab",
+            ()-> new SlabBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_ANDESITE_BRICKS_WALL = registerBlock("polished_andesite_bricks_wall",
+            ()-> new WallBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_ANDESITE_WALL = registerBlock("polished_andesite_wall",
+            ()-> new WallBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+
+
     public static final RegistryObject<Block> POLISHED_DIORITE_BRICKS = registerBlock("polished_diorite_bricks",
             ()-> new Block(BlockBehaviour.Properties.of(Material.STONE)
                     .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_DIORITE_BRICKS_STAIRS = registerBlock("polished_diorite_bricks_stairs",
+            ()-> new StairBlock(() -> ModBlocks.POLISHED_GRANITE_BRICKS.get().defaultBlockState() ,
+                    BlockBehaviour.Properties.of(Material.STONE).strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)),
+            ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_DIORITE_BRICKS_SLAB = registerBlock("polished_diorite_bricks_slab",
+            ()-> new SlabBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_DIORITE_BRICKS_WALL = registerBlock("polished_diorite_bricks_wall",
+            ()-> new WallBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+    public static final RegistryObject<Block> POLISHED_DIORITE_WALL = registerBlock("polished_diorite_wall",
+            ()-> new WallBlock(BlockBehaviour.Properties.of(Material.STONE)
+                    .strength(1.5F, 6.0F).requiresCorrectToolForDrops().sound(SoundType.STONE)), ModCreativeModeTab.MTIWTA_TAB);
+
+
 
     private static <T extends Block>RegistryObject<T> registerBlock(String name, Supplier<T> block, CreativeModeTab tab){
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
